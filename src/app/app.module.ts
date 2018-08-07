7dd150d5137b9245803cfd86847ebf7bf9bd7c32
MDO <dorobisz.mateusz@o2.pl>
2018-08-07 12:04:39 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+
+const Routes = [
+    {
+      path: '',
+      redirectTo: 'posts',
+      pathMatch: 'full'
+    },
+    {
+      path: 'posts', component: PostsComponent
+    }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +22,9 @@ import { PostsComponent } from './posts/posts.component';
     PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(Routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  providers: [PostsService]
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: any = [];
+
+  constructor(private postService: PostsService) { }
 
   ngOnInit() {
+    this.postService.gatAllPosts().subscribe(posts => {
+      this.posts = posts;
+    })
   }
-
 }
